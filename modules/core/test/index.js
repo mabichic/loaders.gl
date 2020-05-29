4775243fbb54cb6b934b03a3d34eecbf83c24268
Ib Green <ib@unfolded.ai>
2020-05-29 09:57:43 -0700

    core: makeResponse utility to unify handling of various input types

--- modules/core/test/index.js
+++ modules/core/test/index.js
@@ -4,6 +4,7 @@ import './javascript-utils/is-type.spec';
 
 import './lib/utils/mime-type-utils.spec';
 import './lib/utils/resource-utils.spec';
+import './lib/utils/response-utils.spec';
 
 import './lib/fetch';
 import './lib/loader-utils';
